5d92a7d8f5d7231b0f99944e99d4911f75b8b568
Jean Regisser <jean.regisser@gmail.com>
2026-01-29 12:26:19 -0700

    chore: prepare package for publishing

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs"],
+  format: ["esm"],
   dts: true,
   clean: true,
   sourcemap: true,
-  target: "node18",
+  target: "node22",
 });
